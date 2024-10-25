e64252f11d7d85564e9b0a1dcee13b715f40d818
hiroki.horiguchi <hiroki.horiguchi.private@gmail.com>
2024-10-26 07:44:02 +0900

    更新漏れ

--- dynamicProgramming/198.house-robber/198.house-robber.kt
+++ dynamicProgramming/198.house-robber/198.house-robber.kt
@@ -7,23 +7,18 @@
 // @lc code=start
 class Solution {
     fun rob(nums: IntArray): Int {
-        return maxOf(even(nums), odd(nums))
-    }
+        if (nums.isEmpty()) return 0
+        if (nums.size == 1) return nums[0]
 
-    fun even(nums: IntArray): Int {
-        var result = 0;
-        for (i in nums.indices step 2) {
-            result += nums[i]
-        }
-        return result
-    }
+        val dp = IntArray(nums.size)
+        dp[0] = nums[0]
+        dp[1] = maxOf(nums[0], nums[1])
+        
+        for (i in 2 until nums.size) {
+            dp[i] = maxOf(dp[i-1], dp[i-2] + nums[i])
+        } 
 
-    fun odd(nums: IntArray): Int {
-        var result = 0;
-        for (i in 1 until nums.size step 2) {
-            result += nums[i]
-        }
-        return result
+        return dp[nums.size - 1]
     }
 }
 // @lc code=end
