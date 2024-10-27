4feaf2ed44bea38232cc01302336ec6a8cf6b35c
hiroki.horiguchi <hiroki.horiguchi.private@gmail.com>
2024-10-27 12:01:49 +0900

    word break 1st 問題文の意味を理解できていないのと、最初の方針に思考が引っ張られるので一旦見送る

---
+++ dynamicProgramming/139.word-break/139.word-break.kt
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=139 lang=kotlin
+ *
+ * [139] Word Break
+ */
+
+// @lc code=start
+class Solution {
+    fun wordBreak(s: String, wordDict: List<String>): Boolean {
+        
+    }
+}
+// @lc code=end
+
